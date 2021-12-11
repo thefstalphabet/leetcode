9ba280d414f1256d67b206acc69fb479abee4051
Akash Patel <69709410+thefstalphabet@users.noreply.github.com>
2021-12-11 18:48:00 +0530

    Add files via upload

---
+++ practice solutions/BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void postOrder(TreeNode* node, vector<int> &ans) {
+        // Basecase
+        if(node == NULL) return;
+        // LEFT, RIGHT, ROOT
+        postOrder(node->left, ans);
+        postOrder(node->right, ans);
+        ans.push_back(node->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        // The vec stores the post order traversal of tree
+        vector<int> ans;
+        postOrder(root, ans);
+        return ans;
+    }
+};
