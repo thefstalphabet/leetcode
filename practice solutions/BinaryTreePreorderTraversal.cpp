f0e87ca703fe2c885f6c17e5e362abee8e7c03fa
Akash Patel <69709410+thefstalphabet@users.noreply.github.com>
2021-12-18 22:21:28 +0530

    Add files via upload

--- practice solutions/BinaryTreePreorderTraversal.cpp
+++ practice solutions/BinaryTreePreorderTraversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Recursive way to traversal
 class Solution {
 private:
     void preOrder(TreeNode* node, vector<int> &ans) {
@@ -24,6 +26,29 @@ public:
         // The vec stores the pre order traversal of tree
         vector<int> ans;
         preOrder(root, ans);
+        return ans;
+    }
+};
+
+// Iterative way to traversal
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans; // to store ans
+        if(root==NULL) return ans; 
+        stack<TreeNode*> st; 
+        st.push(root);
+        while(!st.empty()){
+            root = st.top(); // accessing the top ele of stack 
+            st.pop();
+            ans.push_back(root->val);
+            if(root->right!=NULL){
+                st.push(root->right);
+            }
+            if(root->left!=NULL){
+                st.push(root->left);
+            }
+        }
         return ans;
     }
 };
