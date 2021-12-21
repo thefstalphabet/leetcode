6a7463d1d5d5d27ebf033bef5e9ee659b7940413
Akash Patel <69709410+thefstalphabet@users.noreply.github.com>
2021-12-21 13:42:27 +0530

    Add files via upload

--- practice solutions/BinaryTreePostorderTraversal.cpp
+++ practice solutions/BinaryTreePostorderTraversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Recursive way to Traversal
 class Solution {
 private:
     void postOrder(TreeNode* node, vector<int> &ans) {
@@ -24,6 +26,52 @@ public:
         // The vec stores the post order traversal of tree
         vector<int> ans;
         postOrder(root, ans);
+        return ans;
+    }
+};
+
+// Iterative way to Traversal using 2 stack
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        // Base Case
+        if(root==NULL) return ans;
+        // two stacks
+        stack<TreeNode*> s1; s1.push(root);
+        stack<int> s2;
+        while(!s1.empty()){
+            root = s1.top();
+            s1.pop();
+            s2.push(root->val);
+            if(root->left!=NULL) s1.push(root->left);
+            if(root->right!=NULL) s1.push(root->right);
+        }
+        // Pushing the values to the vec
+        while(!s2.empty()){
+            ans.push_back(s2.top());
+            s2.pop();
+        }
+        return ans;
+    }
+};
+
+// Iterative way to Traversal using 1 stack
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        // Base Case
+        if(root==NULL) return ans;
+        stack<TreeNode*> s1; s1.push(root);
+        while(!s1.empty()){
+            root = s1.top();
+            s1.pop();
+            ans.push_back(root->val);
+            if(root->left!=NULL) s1.push(root->left);
+            if(root->right!=NULL) s1.push(root->right);
+        }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
