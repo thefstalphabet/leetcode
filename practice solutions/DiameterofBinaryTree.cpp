bb1af713af8f3f31fba59dbbfe605f52677efe13
Akash Patel <69709410+thefstalphabet@users.noreply.github.com>
2021-12-30 11:12:58 +0530

    Add files via upload

---
+++ practice solutions/DiameterofBinaryTree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private: int find(TreeNode* node, int &diameter){
+    // when node is null
+    if(node==NULL) return 0;
+    // if not then
+    // lh is the left height of a sub tree
+    // rh is the right height of a sub tree
+    int lh = find(node->left, diameter);
+    int rh = find(node->right, diameter);
+    // the var contain the longest path passing via node
+    diameter = max(diameter, lh + rh);
+    return 1 + max(lh, rh);
+}
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        find(root, diameter);
+        return diameter;
+    }
+};
